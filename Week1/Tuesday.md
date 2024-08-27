8fbe6a216a38a35edc02114d3522a8fff063b8b4
Rylei Mindrum <rymindrum@gmail.com>
2024-08-27 10:56:31 -0600

    Starter code and basic notes

--- Week1/Tuesday.md
+++ Week1/Tuesday.md
@@ -1,1 +1,5 @@
 # Notes: Tuesday, August 27, 2024
+
+## Linked Lists Review
+A list that is connected \
+Separated into Nodes, by pointer/reference to next node \
---
+++ Week1/tues.java
@@ -0,0 +1,18 @@
+private static class Node<E> {
+    E element;
+    Node<E> next;
+
+    public Node(E element) {
+        this(element, null);
+    }
+
+    public Node(E element, Node<E> n) {
+        private Node<E> head, tail;
+        private int size;
+    }
+}
+
+public state class LinkedzList {
+    private Node<E> head, tail;
+    private int size;
+}
